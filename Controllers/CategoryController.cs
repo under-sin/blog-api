b5d6d34df272a2bf797cf9434d054e704a2cb459
under-sin <andersonvieira818@yahoo.com.br>
2022-12-07 08:46:43 -0300

    Commit inicial

---
+++ Controllers/CategoryController.cs
@@ -0,0 +1,22 @@
+﻿using Blog.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Blog.Controllers;
+
+[ApiController]
+public class CategoryController : ControllerBase
+{
+    [HttpGet("v1/categories")]
+    public IActionResult Get([FromServices] BlogDataContext context)
+    {
+        var categories = context.Categories.ToList();
+        return Ok(categories);
+    }
+    
+    [HttpGet("v2/categories")]
+    public IActionResult Get2([FromServices] BlogDataContext context)
+    {
+        var categories = context.Categories.ToList();
+        return Ok(categories);
+    }
+}
