a359a587d43a5b7c69c8c6fffde1365fca86a7ff
under-sin <andersonvieira818@yahoo.com.br>
2022-12-26 19:44:52 -0300

    AccountController

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,23 @@
+using Blog.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Blog.Controllers;
+
+[ApiController]
+public class AccountController : ControllerBase
+{
+    // private readonly TokenService _tokenService;
+    //
+    // public AccountController(TokenService tokenService)
+    // {
+    //     _tokenService = tokenService;
+    // }
+
+    [HttpPost("v1/login")]
+    public IActionResult Login([FromServices] TokenService tokenService)
+    {
+        var token = tokenService.GenerateToken(null);
+        return Ok(token);
+    }
+
+}
